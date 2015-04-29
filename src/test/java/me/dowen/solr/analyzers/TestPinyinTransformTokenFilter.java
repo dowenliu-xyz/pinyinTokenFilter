120306b47f171f40f1e21d0aed7c0d36771b3327
liufl <hawkdowen@126.com>
2015-04-29 14:00:21 +0800

    升级到lucene5.1.0，版本号升级为1.1.0

--- src/test/java/me/dowen/solr/analyzers/TestPinyinTransformTokenFilter.java
+++ src/test/java/me/dowen/solr/analyzers/TestPinyinTransformTokenFilter.java
@@ -1,10 +1,6 @@
 package me.dowen.solr.analyzers;
 
-import static org.junit.Assert.*;
-
-import java.io.IOException;
-import java.io.StringReader;
-
+import org.apache.lucene.analysis.BaseTokenStreamTestCase;
 import org.apache.lucene.analysis.MockTokenizer;
 import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
 import org.apache.lucene.analysis.tokenattributes.OffsetAttribute;
@@ -12,33 +8,36 @@ import org.apache.lucene.analysis.tokenattributes.PositionIncrementAttribute;
 import org.apache.lucene.analysis.tokenattributes.TypeAttribute;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
-@SuppressWarnings("unused")
-@RunWith(com.carrotsearch.randomizedtesting.RandomizedRunner.class)
-public class TestPinyinTransformTokenFilter {
+import java.io.IOException;
+import java.io.StringReader;
 
-	private PinyinTransformTokenFilter filter;
+public class TestPinyinTransformTokenFilter extends BaseTokenStreamTestCase {
 
-	@Before
-	public void before() {
-		MockTokenizer tokenizer = new MockTokenizer(new StringReader("和平 重量 and 中国"));
-		this.filter = new PinyinTransformTokenFilter(tokenizer);
-	}
+    private PinyinTransformTokenFilter filter;
 
-	@Test
-	public void test() throws IOException {
-		this.filter.reset();
-		int position = 0;
-		while (this.filter.incrementToken()) {
-			CharTermAttribute termAtt = this.filter.getAttribute(CharTermAttribute.class);
-			String token = termAtt.toString();
-			int increment = this.filter.getAttribute(PositionIncrementAttribute.class).getPositionIncrement();
-			position += increment;
-			OffsetAttribute offset = this.filter.getAttribute(OffsetAttribute.class);
-			TypeAttribute type = this.filter.getAttribute(TypeAttribute.class);
-			System.out.println(position + "[" + offset.startOffset() + "," + offset.endOffset() + "} (" + type.type() + ") " + token);
-		}
-	}
+    @Before
+    public void before() throws IOException {
+        MockTokenizer tokenizer = new MockTokenizer();
+        tokenizer.setReader(new StringReader("和平 重量 and 中国"));
+        this.filter = new PinyinTransformTokenFilter(tokenizer);
+    }
+
+    @Test
+    public void test() throws IOException {
+        this.filter.reset();
+        int position = 0;
+        while (this.filter.incrementToken()) {
+            CharTermAttribute termAtt = this.filter.getAttribute(CharTermAttribute.class);
+            String token = termAtt.toString();
+            int increment = this.filter.getAttribute(PositionIncrementAttribute.class).getPositionIncrement();
+            position += increment;
+            OffsetAttribute offset = this.filter.getAttribute(OffsetAttribute.class);
+            TypeAttribute type = this.filter.getAttribute(TypeAttribute.class);
+            System.out.println(position + "[" + offset.startOffset() + "," + offset.endOffset() + "} (" + type
+                    .type() + ") " + token);
+        }
+        assertTrue(position == 4);
+    }
 
 }
